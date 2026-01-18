4a2c993e82222cfd28b8594584ab275989a2878d
D4niel <khacduytvt123@gmail.com>
2026-01-18 14:38:47 +0700

    build: configure project for GitHub Pages deployment by adding homepage and setting webpack public path.

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: 'auto',
+    publicPath: '/Image3D/',
   },
   plugins: [
     new HtmlWebpackPlugin({
